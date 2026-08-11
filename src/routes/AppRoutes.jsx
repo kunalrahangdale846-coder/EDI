41f5487892c12a16482e90b0716fd6afde5dfc15
NAGESH PATTHE <nageshpatthe83@gmail.com>
2026-08-11 18:51:16 +0530

    Add Leaderboard route with ProtectedRoute wrapperConnect leaderboard page to route

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -5,6 +5,7 @@ import { ROUTES } from "../utils/constants.js";
 import Landing from "../pages/Landing.jsx";
 import Login from "../pages/Login.jsx";
 import Dashboard from "../pages/Dashboard.jsx";
+import Leaderboard from "../pages/Leaderboard.jsx";
 
 // TODO(teammates): swap these placeholders for the real page components
 // as CreateHackathon.jsx, SubmitProject.jsx, EvaluationReport.jsx,
@@ -45,7 +46,14 @@ function AppRoutes() {
       <Route path={ROUTES.CREATE_HACKATHON} element={<ComingSoon title="Create Hackathon" />} />
       <Route path={ROUTES.SUBMIT_PROJECT} element={<ComingSoon title="Submit Project" />} />
       <Route path={ROUTES.EVALUATION_REPORT} element={<ComingSoon title="Evaluation Report" />} />
-      <Route path={ROUTES.LEADERBOARD} element={<ComingSoon title="Leaderboard" />} />
+      <Route
+  path={ROUTES.LEADERBOARD}
+  element={
+    <ProtectedRoute>
+      <Leaderboard />
+    </ProtectedRoute>
+  }
+/>
       <Route path={ROUTES.PROFILE} element={<ComingSoon title="Profile" />} />
 
       <Route path="*" element={<Navigate to={ROUTES.HOME} replace />} />
